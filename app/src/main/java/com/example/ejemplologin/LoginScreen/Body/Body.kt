9a41fe4f5559c3558d9ab4fed53bc2c47f1934d0
luissancar <luissancar@yahoo.es>
2023-11-10 18:17:47 +0100

    tres

--- app/src/main/java/com/example/ejemplologin/LoginScreen/Body/Body.kt
+++ app/src/main/java/com/example/ejemplologin/LoginScreen/Body/Body.kt
@@ -1,14 +1,18 @@
 package com.example.ejemplologin.LoginScreen.Body
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -23,10 +27,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.util.PatternsCompat
 import com.example.ejemplologin.R
 
 
@@ -38,15 +42,94 @@ fun Body(modifier: Modifier) {
     var password by rememberSaveable {
         mutableStateOf("")
     }
+    var isLoginEnable by rememberSaveable {
+        mutableStateOf(false)
+    }
     Column(modifier = modifier) {
-        ImageLogo()
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
-        Email(email) { email = it }
+        Email(email) {
+            email = it
+            if (password.length > 0 && email.length > 0 && isValidEmail(email))
+                isLoginEnable = true
+            else
+                isLoginEnable = false
+        }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password) {
+            password = it
+            if (password.length > 0 && email.length > 0 && isValidEmail(email))
+                isLoginEnable = true
+            else
+                isLoginEnable = false
+        }
         Spacer(modifier = Modifier.size(16.dp))
-        Password(password) { password = it }
+        LoginButton(isLoginEnable)
         Spacer(modifier = Modifier.size(16.dp))
-        ForgotPassword(Modifier.align(Alignment.End))
+        LoginDivider()
+        Spacer(modifier = Modifier.size(32.dp))
+        SocialLogin()
+        Spacer(modifier = Modifier.size(16.dp))
+        ForgotPassword(Modifier.align(Alignment.CenterHorizontally))
 
+    }
+}
+
+@Composable
+fun SocialLogin() {
+    Row(
+        Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fb),
+            contentDescription = "Social",
+            modifier = Modifier.size(16.dp)
+        )
+        Text(
+            text = "Log in with Facebook",
+            fontSize = 11.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            color = Color(0XFF4EA8E9)
+        )
+    }
+}
+
+@Composable
+fun LoginDivider() {
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+        Text(
+            text = "OR",
+            modifier = Modifier.padding(horizontal = 18.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFB5B5B5)
+        )
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean) {
+    Button(
+        onClick = { /*TODO*/ }, enabled = loginEnable,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 18.dp),
+    ) {
+        Text(text = "Log In")
     }
 }
 
@@ -54,9 +137,9 @@ fun Body(modifier: Modifier) {
 fun ForgotPassword(modifier: Modifier) {
     Text(
         text = "Forgot Password?",
-        fontSize = 12.sp,
+        fontSize = 8.sp,
         fontWeight = FontWeight.Bold,
-        color = Color(0xFF4EA8E9),
+        color = Color(0xFFB5B5B5),
         modifier = modifier
     )
 }
@@ -64,28 +147,69 @@ fun ForgotPassword(modifier: Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Password(password: String, onTextChanged: (String) -> Unit) {
-    TextField(
-        value = password,
-        onValueChange = { onTextChanged(it) },
-        trailingIcon = {
-            Icon(
-                painter = painterResource(id = com.google.android.material.R.drawable.design_ic_visibility_off),
-                contentDescription = "View"
-            )
-        },
-        visualTransformation = PasswordVisualTransformation(),
+    var showPassword by rememberSaveable {
+        mutableStateOf(false)
+    }
 
+    if (showPassword)
+        TextField(
+            value = password,
+            onValueChange = { onTextChanged(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = com.google.android.material.R.drawable.design_ic_visibility),
+                    contentDescription = "View",
+                    modifier = Modifier.clickable { showPassword = !showPassword }
+                )
+            }
+            //visualTransformation = PasswordVisualTransformation(),
         )
+    else
+        TextField(
+            value = password,
+            onValueChange = { onTextChanged(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = com.google.android.material.R.drawable.design_ic_visibility_off),
+                    contentDescription = "View",
+                    modifier = Modifier.clickable { showPassword = !showPassword }
+                )
+            },
+            visualTransformation = PasswordVisualTransformation(),
+        )
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Email(email: String, onTextChanged: (String) -> Unit) {
-    TextField(value = email, onValueChange = { onTextChanged(it) })
+    TextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 18.dp),
+        label = { Text(text = "email") }
+    )
 }
 
 
 @Composable
-fun ImageLogo() {
-    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "logo")
+fun ImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "logo",
+        modifier = modifier
+    )
+}
+
+
+fun isValidEmail(email: String): Boolean {
+    return PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
 }
